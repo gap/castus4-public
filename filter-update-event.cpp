61e1838434eae85ac2d72ef46bb9065a1d93dc98
Alex Elsayed <eternaleye@gmail.com>
2019-03-29 11:26:24 -0700

    Refactor filter-update-event to match spec better

--- filter-update-event.cpp
+++ filter-update-event.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <ctime>
 
+#include <boost/filesystem.hpp>
+
 #include <castus4-public/schedule.h>
 #include <castus4-public/schedule_object.h>
 
@@ -8,9 +10,13 @@
 #include "utils_schedule.h"
 
 using namespace std;
+namespace fs = boost::filesystem;
 
-int main() {
-	Castus4publicSchedule schedule;
+using Sched = Castus4publicSchedule;
+using ideal_time_t = Sched::ideal_time_t;
+
+int main(int argc, char** argv) {
+	Sched schedule;
     load(schedule);
 
     // Step 1: Get the current time
@@ -18,7 +24,7 @@ int main() {
     clock_gettime(CLOCK_REALTIME, &current_time);
 
     // How far into the schedule we are (predeclared due to initialization within a conditional)
-    Castus4publicSchedule::ideal_time_t sched_offset;
+    ideal_time_t sched_offset;
 
     switch (schedule.schedule_type) {
         case C4_SCHED_TYPE_DAILY:
@@ -27,7 +33,9 @@ int main() {
             // Clear the date components because we only care about the time side
             cal.tm_year = cal.tm_mon = cal.tm_mday = 0;
             // Convert back to seconds, and make it compatible with CASTUS' time format
-            sched_offset = (Castus4publicSchedule::ideal_time_t)mktime(&cal) * Castus4publicSchedule::ideal_microsec_per_sec;
+            sched_offset =
+                cal.tm_hour * Sched::ideal_hour +
+                cal.tm_min  * Sched::ideal_minute;
             break;
         default:
             // Only daily schedules are supported currently
@@ -35,8 +43,12 @@ int main() {
     }
 
     // Step 2: Find the first block whose start..end range contains that time
-    auto result = find_if(schedule.schedule_blocks.begin(), schedule.schedule_blocks.end(), [sched_offset](Castus4publicSchedule::ScheduleBlock& block) {
-        return (block.getStartTime() <= sched_offset && block.getEndTime() >= sched_offset);
+    auto result = find_if(schedule.schedule_blocks.begin(), schedule.schedule_blocks.end(), [sched_offset](Sched::ScheduleBlock& block) {
+        return (
+            block.getStartTime() <= sched_offset &&
+            block.getEndTime()   >= sched_offset &&
+            string(block.getValue("trigger")) == "charter_event"
+        );
     });
 
     // If no matching blocks exist, our work is done; however, since the user
@@ -49,25 +61,61 @@ int main() {
     // Bind the block to a reference to tidy up the remaining code
     auto& block = *result;
 
-    // Step 3: Calculate how far into the block we are
-    auto time_offset = sched_offset - block.getStartTime();
-
-    // Step 4.1: Move the targeted items down by the difference between the block's start and the current time
-    for (auto& item : schedule.schedule_items) {
-        if (is_valid(item) && in_block(item, block) &&
-            // All well-formed triggered items will start at the top of the block
-            item.getStartTime() == block.getStartTime() &&
-            // All well-formed triggered items will have zero duration
-            item.getStartTime() == item.getEndTime()) {
-
-            item.setStartTime(item.getStartTime() + time_offset);
-            item.setEndTime(  item.getEndTime()   + time_offset);
+    // Step 3: Find the items that should play as part of this schedule
+    // The base directory for the daily schedules (and files) of this yearly schedule
+    fs::path base;
+    bool base_initialized = false;
+    for (int arg_idx = 1; arg_idx < argc; ++arg_idx) {
+        // The path to the schedule is given as `--file <path>`
+        if (string(argv[arg_idx]) == "--file" && arg_idx + 1 < argc) {
+            // Capture the path
+            base = fs::path(argv[arg_idx + 1]);
+            // And replace the schedule with the directory the videos are in
+            base.remove_leaf() /= block.getBlockName();
+            base_initialized = true;
         }
+    }
+    if (!base_initialized) {
+        return 3;
+    }
+
+    // Collect the files in the directory
+    vector<fs::path> files;
+    transform(
+        fs::directory_iterator(base),
+        fs::directory_iterator(),
+        files.begin(),
+        [](fs::directory_entry& entry){ return entry.path(); }
+    );
+    // Remove anything that isn't a non-hidden m2ts
+    remove_if(files.begin(), files.end(), [](fs::path& path){
+        if (path.extension() != "m2ts") {
+            return true;
+        } else if (path.leaf().string().substr(0, 1) == ".") {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    // Sort them
+    sort(files.begin(), files.end());
+
+    // Give each one a trivial duration, so that they have a clear
+    // ordering when Jonathan's code sorts them
+    ideal_time_t bias = 0;
+    // Step 4.1: Create items (at the appropriate time) for the event
+    for (auto& path : files) {
+        Sched::ScheduleItem item{C4_SCHED_TYPE_DAILY};
+        item.setItem(path.string());
+        item.setStartTime(sched_offset + bias);
+        bias += Sched::ideal_microsecond;
+        item.setEndTime(  sched_offset + bias);
+        item.setValue("trigger", "charter_event");
     }
 
     // Step 4.2: Correct the targeted items' durations
     for (auto& item : schedule.schedule_items) {
-        if (is_valid(item) && in_block(item, block)) {
+        if (is_valid(item) && string(item.getValue("trigger")) == "charter_event") {
             // Update the duration information
             update_timing(item);
         }
@@ -85,8 +133,8 @@ int main() {
         // Bind the current item
         auto& current = *tmp;
 
-        if (is_valid(current) && in_block(current, block) &&
-            is_valid(next)    && in_block(next,    block)) {
+        if (is_valid(current) && string(current.getValue("trigger")) == "charter_event" &&
+            is_valid(next)    && string(   next.getValue("trigger")) == "charter_event") {
 
             // Shift the `next` item down to no longer overlap with `current`
             ripple_one(current, next);
@@ -103,6 +151,11 @@ int main() {
                 next.setEndTime(block.getEndTime());
             }
         }
+    }
+
+    // Clear out the trigger markers on old items
+    for (auto& item : schedule.schedule_items) {
+        item.deleteValue("trigger");
     }
 
     write(schedule);
